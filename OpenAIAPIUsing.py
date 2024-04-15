5765b04b165e23441d050aa1ec659f8c2e4cdcdd
Vitor Pietrobom <vitorpietrobom@gmail.com>
2024-04-15 15:52:54 -0300

    fix: fixed error where reviews would be empty

--- OpenAIAPIUsing.py
+++ OpenAIAPIUsing.py
@@ -1,7 +1,10 @@
 from openai import OpenAI
 
 def generate_summary(place_data):
-    formatted_reviews = format_reviews(place_data['reviews'])
+    try:
+        formatted_reviews = format_reviews(place_data['reviews'])
+    except KeyError:
+        formatted_reviews = "N/A"
     text_input = f"""
         Place: {place_data['name']}
         Address: {place_data['formatted_address']}
@@ -10,13 +13,14 @@ def generate_summary(place_data):
         Opening Hours: {', '.join(place_data.get('opening_hours', 'N/A'))}
 
         Please summarize the key points about this place, separating in its positive and negative aspects.
+        The response should be in the same language of the reviews.
         """
     client = OpenAI()
 
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4-turbo",
         messages=[
-            {"role": "system", "content": "The following is a summary of the key points about this place, separated in positive and negative bullet points:"},
+            {"role": "system", "content": "You will be provided with informations about a place, please make a summary of the key points about this place, separating it in positive and negative bullet points"},
             {"role": "user", "content": text_input}
         ]
     )
